fb9a8e822f80b76e1da9e1c319e513b1a82feffd
Tomasz Jonik <tom.jonik@hotmail.com>
2016-01-22 03:17:38 +0000

    Added delete bucket feature

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ MAINTAINER Tomasz Jonik <tomasz@hurricane.works>
 VOLUME /data
 COPY ./data /data
 CMD chmod +x /data/run.sh
-
+WORKDIR /data
 RUN /bin/bash
--- data/run.sh
+++ data/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+prefix=${S3_PREFIX:=$1}
+bucket=${S3_BUCKET:=$2}
+
 /bin/sed \
     -e "s|AWS_KEY|$AWS_KEY|" \
     -e "s|AWS_SECRET|$AWS_SECRET|" \
-    -e "s|ES_HOST|$ES_HOST|" \
-    -e "s|ES_PORT|$ES_PORT|" \
-    -e "s|S3_BUCKET|$S3_BUCKET|" \
-    -e "s|S3_PREFIX|$S3_PREFIX|" \
-    /data/logstash.conf > /data/build.conf
+    -e "s|S3_BUCKET|$bucket|" \
+    -e "s|S3_PREFIX|$prefix|" \
+    -e "s|ES_HOST|${ES_HOST:=elasticsearch}|" \
+    -e "s|ES_PORT|${ES_PORT:=9200}|" \
+    -e "s|S3_DELETE|${S3_DELETE:=false}|" \
+    -e "s|S3_TEMPORARY_DIRECTORY|${S3_TEMPORARY_DIRECTORY:=/tmp/$S3_PREFIX}|" \
+    logstash.conf > build.conf
+
+cat build.conf
